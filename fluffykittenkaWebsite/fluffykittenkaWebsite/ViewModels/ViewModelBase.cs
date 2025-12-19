f25c2d071f710e6b1f27db5298f5b207a4a0fb54
meow1-tech <nitzanaaranovich@gmail.com>
2025-12-19 17:09:30 +0200

    meow

---
+++ fluffykittenkaWebsite/fluffykittenkaWebsite/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace fluffykittenkaWebsite.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
---
+++ fluffykittenkaWebsite/fluffykittenkaWebsite/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace fluffykittenkaWebsite.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
